f13480725de4b7e663c36755a389656178dcd606
Protus <prolink7544@gmail.com>
2023-04-18 06:49:20 -0700

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb+srv://prolink7544:0723557337@yolo.mzzt1uq.mongodb.net/?retryWrites=true/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
